00fb4d48e30a9647117262bec69d6cc3ab75a142
jacordobah <jacordobah@unal.edu.co>
2025-01-16 12:26:25 -0500

    inicializacion del proyecto

---
+++ src/main/java/com/jalcoh/lieratura/LieraturaApplication.java
@@ -0,0 +1,13 @@
+package com.jalcoh.lieratura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LieraturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LieraturaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jalcoh/lieratura/LieraturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jalcoh.lieratura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LieraturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
